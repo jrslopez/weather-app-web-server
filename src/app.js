9a6c357f5ae4428b8ee33b4fe5d1e8c9d1083d8d
jrslopez <lopez.jamesrussel@gmail.com>
2022-07-20 07:34:53 -0400

    setup handlerbars

--- src/app.js
+++ src/app.js
@@ -4,16 +4,28 @@ const express = require("express")
 const app = express()
 const publicDirectoryPath = path.join(__dirname, "../public")
 
+app.set("view engine", "hbs")
 app.use(express.static(publicDirectoryPath))
 
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "James Lopez",
+  })
+})
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "James Lopez",
+  })
+})
+
 app.get("/help", (req, res) => {
-  res.send([
-    {
-      name: "James",
-      age: 25,
-    },
-    { name: "Cammylle", age: 25 },
-  ])
+  res.render("help", {
+    title: "Help Page",
+    name: "James Lopez",
+  })
 })
 
 app.listen(3000, () => {
