7a079ed77f660d85e73f4f46023d71220374fd34
jrslopez <lopez.jamesrussel@gmail.com>
2022-07-25 01:36:58 -0400

    setup partials

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 const path = require("path")
 const express = require("express")
+const hbs = require("hbs")
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, "../public")
 
+// Define paths for express config
+const publicDirectoryPath = path.join(__dirname, "../public")
+const viewsPath = path.join(__dirname, "../templates/views")
+const partialsPath = path.join(__dirname, "../templates/partials")
+
+// Setup handlebars engine and views location
 app.set("view engine", "hbs")
+app.set("views", viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get("", (req, res) => {
@@ -26,6 +36,10 @@ app.get("/help", (req, res) => {
     title: "Help Page",
     name: "James Lopez",
   })
+})
+
+app.get("*", (req, res) => {
+  res.send("My 404 page")
 })
 
 app.listen(3000, () => {
