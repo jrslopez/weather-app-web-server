00a62e85f680f0f322def7115e435a997127e357
James Lopez <james@jamess-mbp.mynetworksettings.com>
2022-07-25 08:54:10 -0400

    Added a UI

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const path = require("path")
 const express = require("express")
 const hbs = require("hbs")
+const forecast = require("./utils/forecast")
+const geocode = require("./utils/geocode")
 
 const app = express()
 
@@ -35,6 +37,48 @@ app.get("/help", (req, res) => {
   res.render("help", {
     title: "Help Page",
     name: "James Lopez",
+  })
+})
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address",
+    })
+  }
+
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error })
+      }
+
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error })
+        }
+
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address,
+        })
+      })
+    }
+  )
+})
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term",
+    })
+  }
+
+  console.log(req.query.rating)
+  res.send({
+    products: [],
   })
 })
 
